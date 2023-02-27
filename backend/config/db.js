b4f15c67ac6c1109c77833e646cb2a2ca672a609
SumitUjjwal <sumitonboard@gmail.com>
2023-02-28 04:54:47 +0530

    establishing connection between server and database

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const connection = mongoose.connect(MONGODB_URL);
+
+module.exports = {
+    connection
+}
